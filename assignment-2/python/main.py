9eab39da33c0c0b7dd57803260dd679ce9abcf36
Teych <tholub99@gmail.com>
2022-10-02 19:33:55 -0400

    Add input args
    Add input args to assignment

--- assignment-2/python/main.py
+++ assignment-2/python/main.py
@@ -1,50 +1,64 @@
+import sys
+import maya.cmds as cmds
+import maya.standalone
 import argparse
 
-#parser = argparse.ArgumentParser(description='This script creates a bunch of cubes.')
-#parser.add_argument('num_cubes', type=int, help="Number of cubes")
-
-#args = parser.parse_args()
-
-import maya.standalone
 maya.standalone.initialize()
 
-import maya.cmds as cmds
+parser = argparse.ArgumentParser(description='This script creates snowman!')
+parser.add_argument('height', metavar='H', type=str,
+                    help="Declare snowman height: tall/short")
+
+parser.add_argument('--nox', dest='hasExtras', action='store_const',
+                    const=False, default=True,
+                    help="Hide extra features")
+
+args = parser.parse_args()
+if(args.height != 'tall' and args.snow_height != 'short'):
+    print('invalid snowman height parameter, must be: [ tall | short ], you gave: ' + args.snow_height)
+    sys.exit(0)
+    
 
 cmds.file(new=True)
-cmds.file(rn=('test.ma'))
+cmds.file(rn=('holub_assignment2.ma'))
 
 #Generate Body
-cmds.polySphere(r=1, n=('bodyLower'))
+if(args.height == 'tall'):
+    cmds.polySphere(r=1.25, n=('bodyLower'))
+    cmds.move(-1.7, y=True)
+    
+cmds.polySphere(r=1, n=('bodyBase'))
 
 cmds.polySphere(r=0.75, n=('bodyUpper'))
 cmds.move(1.4, y=True)
-#Generate Hat (brim + top)
+
+#Generate Hat
 cmds.polyCylinder(r=0.7, h=0.1, n=('hatBrim'))
 cmds.move(2, y=True)
 
 cmds.polyCylinder(r=0.4, h=1, n=('hatCrown'))
 cmds.move(2.5, y=True)
-#Generate Buttons (3 sphere on lower)
-cmds.polySphere(r=0.1, n=('buttonLower'))
-cmds.move(0.95, z=True)
 
-cmds.polySphere(r=0.1, n=('buttonMiddle'))
-cmds.move(0, 0.25, 0.9)
+if(args.hasExtras):
+    #Generate Buttons
+    cmds.polySphere(r=0.1, n=('buttonLower'))
+    cmds.move(0.95, z=True)
 
-cmds.polySphere(r=0.1, n=('buttonUpper'))
-cmds.move(0, 0.5, 0.8)
-#Generate Face (eyes + nose)
-cmds.polySphere(r=0.1, n=('eyeLeft'))
-cmds.move(0.25, 1.7, 0.6)
+    cmds.polySphere(r=0.1, n=('buttonMiddle'))
+    cmds.move(0, 0.25, 0.9)
 
-cmds.polySphere(r=0.1, n=('eyeRight'))
-cmds.move(-0.25, 1.7, 0.6)
+    cmds.polySphere(r=0.1, n=('buttonUpper'))
+    cmds.move(0, 0.5, 0.8)
 
-cmds.polyCone(r=0.1, h=0.5, n=('nose'))
-cmds.move(0, 1.5, 0.95)
-cmds.rotate('85deg', 0, 0)
+    #Generate Face
+    cmds.polySphere(r=0.1, n=('eyeLeft'))
+    cmds.move(0.25, 1.7, 0.6)
 
-print("Meshes in the Maya scene:")
-print(maya.cmds.ls(geometry=True))
+    cmds.polySphere(r=0.1, n=('eyeRight'))
+    cmds.move(-0.25, 1.7, 0.6)
+
+    cmds.polyCone(r=0.1, h=0.5, n=('nose'))
+    cmds.move(0, 1.5, 0.95)
+    cmds.rotate('85deg', 0, 0)
 
 cmds.file(save=True, typ=('mayaAscii'))
--- assignment-2/python/mayapyCmdHelp.py
+++ assignment-2/python/mayapyCmdHelp.py
@@ -1,11 +1,12 @@
 import maya.standalone
-maya.standalone.initialize(name='python')
 import maya.cmds as cmds
 import argparse
+
+maya.standalone.initialize(name='python')
 
 parser = argparse.ArgumentParser(description='Get maya command help')
 parser.add_argument('cmdName', type=str, help="Command to Show Help")
 
 args = parser.parse_args()
-helpinfo = cmds.help(args.cmdName)
-print(helpinfo)
+helpInfo = cmds.help(args.cmdName)
+print(helpInfo)
