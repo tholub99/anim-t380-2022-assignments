c39aadb3f7255546cf763dd260b5c13109163eee
Teych <tholub99@gmail.com>
2022-10-03 11:31:40 -0400

    mitigate magic numbers
    Removes (within reason) bad magic numbers

--- assignment-2/python/main.py
+++ assignment-2/python/main.py
@@ -23,41 +23,56 @@ cmds.file(new=True)
 cmds.file(rn=('holub_assignment2.ma'))
 
 #Generate Body
+baseRad = 1.25
+coreRad = 1
+headRad = 0.75
+
 if(args.height == 'tall'):
-    cmds.polySphere(r=1.25, n=('bodyLower'))
+    cmds.polySphere(r=baseRad, n=('bodyBase'))
     cmds.move(-1.7, y=True)
     
-cmds.polySphere(r=1, n=('bodyBase'))
+cmds.polySphere(r=coreRad, n=('bodyCore'))
 
-cmds.polySphere(r=0.75, n=('bodyUpper'))
+cmds.polySphere(r=headRad, n=('bodyHead'))
 cmds.move(1.4, y=True)
 
 #Generate Hat
-cmds.polyCylinder(r=0.7, h=0.1, n=('hatBrim'))
+brimRad = 0.7
+brimHeight = 0.1
+crownRad = 0.4
+crownHeight = 1
+
+cmds.polyCylinder(r=brimRad, h=brimHeight, n=('hatBrim'))
 cmds.move(2, y=True)
 
-cmds.polyCylinder(r=0.4, h=1, n=('hatCrown'))
+cmds.polyCylinder(r=crownRad, h=crownHeight, n=('hatCrown'))
 cmds.move(2.5, y=True)
 
 if(args.hasExtras):
     #Generate Buttons
-    cmds.polySphere(r=0.1, n=('buttonLower'))
+    buttonRad = 0.1
+    
+    cmds.polySphere(r=buttonRad, n=('buttonLower'))
     cmds.move(0.95, z=True)
 
-    cmds.polySphere(r=0.1, n=('buttonMiddle'))
+    cmds.polySphere(r=buttonRad, n=('buttonMiddle'))
     cmds.move(0, 0.25, 0.9)
 
-    cmds.polySphere(r=0.1, n=('buttonUpper'))
+    cmds.polySphere(r=buttonRad, n=('buttonUpper'))
     cmds.move(0, 0.5, 0.8)
 
     #Generate Face
-    cmds.polySphere(r=0.1, n=('eyeLeft'))
+    eyeRad = 0.1
+    noseRad = 0.1
+    noseHeight = 0.5
+    
+    cmds.polySphere(r=eyeRad, n=('eyeLeft'))
     cmds.move(0.25, 1.7, 0.6)
 
-    cmds.polySphere(r=0.1, n=('eyeRight'))
+    cmds.polySphere(r=eyeRad, n=('eyeRight'))
     cmds.move(-0.25, 1.7, 0.6)
 
-    cmds.polyCone(r=0.1, h=0.5, n=('nose'))
+    cmds.polyCone(r=noseRad, h=noseHeight, n=('nose'))
     cmds.move(0, 1.5, 0.95)
     cmds.rotate('85deg', 0, 0)
 
