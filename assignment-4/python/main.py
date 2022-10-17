333649c4f7cdbaa063633a862697e033f29617b5
Teych <tholub99@gmail.com>
2022-10-17 13:42:36 -0400

    Swap to regex, validate input, iterate files

--- assignment-4/python/main.py
+++ assignment-4/python/main.py
@@ -1,37 +1,74 @@
 import os
+import re
+import sys
 import maya.cmds as cmds
 import maya.standalone
+import argparse
 
 maya.standalone.initialize()
 
+parser = argparse.ArgumentParser(description='This script creates a snowman!')
+parser.add_argument('fileName', metavar='f', type=str,
+                    help="Declare file name")
 
-BASE_NAME = 'asset.model.tholub'
-'''
-Open File
-If file exists:
-Parse Name
-If not:
-Create new file starting at 1 (0?)
-'''
-if(cmd.file(ex=(fileName))):
-    cmd.file(o=(fileName))
-else:
-    print('Creating new file')
-    cmd.file(new=True)
-    cmd.file(rn=(os.path.join(os.getcwd(), BASE_NAME + '.1')))
+parser.add_argument('--new', dest='isNew', action='store_const',
+                    const=True, default=False,
+                    help="Create New File")
+
+args = parser.parse_args()
+def isValidName(fileName, isNew):
+    if(isNew):
+        res = re.fullmatch('^(\w+\.){2}(\w+)$', fileName)
+        if(res == None):
+            print('Invalid name for new file, follow the format:\nassetName.type.user')
+            return False
+    else:
+        res = re.fullmatch('^(\w+\.){3}(\d+)(\.ma)$', fileName)
+        if(res == None):
+            print('Invalid name for iterable file, follow the format:\nassetName.type.user.#.ma')
+            return False
+    
+    return True
+
+    
+def CreateNewFile(fileName, cwd):
+    cmds.file(new=True)
+    cmds.file(rn=(os.path.join(cwd, fileName + '.1.ma')))
     
 '''
-Increment File name
+Iterate File name
 '''
-splitName = cmd.file(sn=True).split('.')
-splitName[3] = str(int(splitName[3]) + 1)
+def IterateFile():
+    fileName = cmds.file(q=True, sn=True)
+    m = re.search('(\d+)\.ma', fileName)
+    nextItr = str(int(m.group(1)) + 1) + '.ma'
+    
+    '''
+    Rename File
+    '''
+    cmds.file(rn=(re.sub('(\d+)\.ma', nextItr, fileName)))
+    
+'''
+Open File or Create New File
+'''
+cwd = os.getcwd()
+if(not isValidName(args.fileName, args.isNew)):
+    sys.exit(1)
 
-'''
-Rename File
-'''
-cmd.file(rn=((os.path.join(os.getcwd(), splitName.join('.')))))
+if(args.isNew):
+    print('Creating New File...')
+    CreateNewFile(args.fileName, cwd)
 
+elif(cmds.file(os.path.join(cwd, args.fileName), q=True, ex=True)):
+    print('Opening File')
+    cmds.file(os.path.join(cwd, args.fileName), o=True)
+    IterateFile()
+else:
+    print('File Not Found')
+    sys.exit(2)
+    
 '''
-Save interated file to dir as mayaAscii
+Save file to dir as mayaAscii
 '''
+print('Saving')
 cmds.file(save=True, typ=('mayaAscii'))
